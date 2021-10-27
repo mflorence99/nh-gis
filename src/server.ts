07239dbcab6a68a0c74171f00d5c476debf3881a
Mark Florence <mflo999@gmail.com>
2021-10-27 01:50:04 -0400

    First pass proxy server

--- src/server.ts
+++ src/server.ts
@@ -2,31 +2,74 @@ import { Compressor } from 'serverx-ts';
 import { CORS } from 'serverx-ts';
 import { FILE_SERVER_OPTS } from 'serverx-ts';
 import { FileServer } from 'serverx-ts';
+import { Handler } from 'serverx-ts';
 import { HttpApp } from 'serverx-ts';
+import { Message } from 'serverx-ts';
+import { Observable } from 'rxjs';
 import { REQUEST_LOGGER_OPTS } from 'serverx-ts';
 import { RequestLogger } from 'serverx-ts';
 import { Route } from 'serverx-ts';
 
 import { createServer } from 'http';
+import { from } from 'rxjs';
+import { mapTo } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { tap } from 'rxjs';
 
 import chalk from 'chalk';
+import fetch from 'node-fetch';
+
+class ProxyServer extends Handler {
+  handle(message$: Observable<Message>): Observable<Message> {
+    return message$.pipe(
+      mergeMap((message: Message): Observable<Message> => {
+        const { request, response } = message;
+        const url = request.query.get('url');
+        return of(message).pipe(
+          mergeMap(() => from(fetch(url))),
+          mergeMap((resp: any) => from(resp.buffer())),
+          tap((buffer) => {
+            response.body = buffer;
+            response.statusCode = 200;
+          }),
+          mapTo(message)
+        );
+      })
+    );
+  }
+}
+
+const fileServerOpts = {
+  provide: FILE_SERVER_OPTS,
+  useValue: { root: __dirname }
+};
+
+const loggerOpts = {
+  provide: REQUEST_LOGGER_OPTS,
+  useValue: { format: 'tiny' }
+};
 
 const routes: Route[] = [
+  {
+    path: '/proxy',
+    methods: ['GET'],
+    handler: ProxyServer,
+    middlewares: [Compressor, CORS, RequestLogger],
+    services: [loggerOpts]
+  },
   {
     path: '/',
     methods: ['GET'],
     handler: FileServer,
     middlewares: [Compressor, CORS, RequestLogger],
-    services: [
-      { provide: REQUEST_LOGGER_OPTS, useValue: { format: 'tiny' } },
-      { provide: FILE_SERVER_OPTS, useValue: { root: __dirname } }
-    ]
+    services: [loggerOpts, fileServerOpts]
   },
   {
     path: '/',
     methods: ['OPTIONS'],
     middlewares: [CORS, RequestLogger],
-    services: [{ provide: REQUEST_LOGGER_OPTS, useValue: { format: 'tiny' } }]
+    services: [loggerOpts]
   }
 ];
 
