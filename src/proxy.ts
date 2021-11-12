724e5f45cde956e1753a05523c28b4501682c31c
Mark Florence <mflo999@gmail.com>
2021-11-12 04:52:24 -0500

    Experiment to cache proxied requests

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,15 +1,19 @@
+import * as fs from 'fs';
+
 import { Handler } from 'serverx-ts';
 import { Injectable } from 'injection-js';
 import { Message } from 'serverx-ts';
 import { Observable } from 'rxjs';
 
 import { from } from 'rxjs';
+import { fromReadableStream } from 'serverx-ts';
 import { mapTo } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { tap } from 'rxjs';
 
 import fetch from 'node-fetch';
+import hash from 'object-hash';
 
 // 👇 a trivial proxy server so that we can use ArcGIS etc
 //    in prodfuction -- ie w/o the Webpack proxy
@@ -20,7 +24,11 @@ export class ProxyServer extends Handler {
     return message$.pipe(
       mergeMap((message: Message): Observable<Message> => {
         const { request, response } = message;
+
+        // 👉 proxied URL is in the query param
         let url = request.query.get('url');
+
+        // 👉 decode any X, Y, Z parameters
         const x = request.query.get('x');
         const y = request.query.get('y');
         const z = request.query.get('z');
@@ -29,17 +37,40 @@ export class ProxyServer extends Handler {
           url = url.replace(/\{y\}/, y);
           url = url.replace(/\{z\}/, z);
         }
+
+        // 🔥 see if we've cached result
+        //    need to check if expire here
+        const fpath = `/tmp/${hash.MD5(url)}.proxy`;
+        const isCached = fs.existsSync(fpath);
+
         return of(message).pipe(
-          mergeMap(() => from(fetch(url))),
-          tap((resp) => {
-            const headers = resp.headers.raw();
-            ['cache-control', 'last-modified', 'etag'].forEach((key) => {
-              if (headers[key]) response.headers[key] = headers[key];
-            });
+          mergeMap(() => {
+            // 👉 read from file system if cached
+            if (isCached) {
+              return fromReadableStream(fs.createReadStream(fpath)).pipe(
+                tap((buffer: Buffer) => {
+                  response.body = buffer;
+                  response.statusCode = 200;
+                })
+              );
+            }
+
+            // 👉 use FETCH to GET the proxied URL if not cached
+            else {
+              return from(fetch(url)).pipe(
+                tap((resp) => {
+                  const headers = resp.headers.raw();
+                  ['cache-control', 'last-modified', 'etag'].forEach((key) => {
+                    if (headers[key]) response.headers[key] = headers[key];
+                  });
+                }),
+                tap((resp) => (response.statusCode = resp.status)),
+                mergeMap((resp) => from(resp.buffer())),
+                tap((buffer) => (response.body = buffer)),
+                tap((buffer) => fs.writeFileSync(fpath, buffer))
+              );
+            }
           }),
-          tap((resp) => (response.statusCode = resp.status)),
-          mergeMap((resp) => from(resp.buffer())),
-          tap((buffer) => (response.body = buffer)),
           mapTo(message)
         );
       })
